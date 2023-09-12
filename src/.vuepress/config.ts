742074dbbce331d365338b233b57f8254ec8912d
dukang <dk@nosugartech.com>
2023-09-12 12:13:03 +0800

    Fix：修改base值为子路径：/front-end-camps/

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from 'vuepress';
 import theme from './theme.js';
 
 export default defineUserConfig({
-	base: '/',
+	base: '/front-end-camps/',
 	lang: 'zh-CN',
 	title: '前端集训营',
 	description: '新时代农民工的集训营',

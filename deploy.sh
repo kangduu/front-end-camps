820fd244ae70b937307f6f6df91f5d5b3e5e359a
dukang <dk@nosugartech.com>
2023-09-13 11:37:56 +0800

    Fix:部署文档

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,11 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+yarn run docs:build
 
 # 进入生成的文件夹
-# cd docs/.vuepress/dist
-cd dist
+cd src/.vuepress/dist
+# cd dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -17,6 +17,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:kangduu/kangduu.github.io.git master
+git push -f git@github.com:kangduu/kangduu.github.io.git master:gh-pages
 
 cd -
